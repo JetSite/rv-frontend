919edac070d8177b9b50e298f288918145600105
Pavel Kotliarov <shpun.0613@gmail.com>
2024-08-20 19:03:17 +0300

    fix build

--- src/utils/getNormalizeDate.ts
+++ src/utils/getNormalizeDate.ts
@@ -1,8 +1,19 @@
 import dayjs from 'dayjs'
 import 'dayjs/locale/ru'
 import 'dayjs/locale/en'
-import 'dayjs/locale/hy-AM'
+// import 'dayjs/locale/hy-AM'
 import { Locale } from '@/i18n-config'
+
+const locale = {
+  name: 'hy-AM',
+  weekdays:
+    'Կիրակի_Երկուշաբթի_Երեքշաբթի_Չորեքշաբթի_Հինգշաբթի_Ուրբաթ_Շաբաթ'.split('_'),
+  months:
+    'Հունվար_Փետրվար_Մարտ_Ապրիլ_Մայիս_Հունիս_Հուլիս_Օգոստոս_Սեպտեմբեր_Հոկտեմբեր_Նոյեմբեր_Դեկտեմբեր'.split(
+      '_',
+    ),
+  // Дополнительно добавьте остальные настройки, если необходимо
+}
 
 type IGetNormalizeDate = (string: string | Date, locale: Locale) => string
 
